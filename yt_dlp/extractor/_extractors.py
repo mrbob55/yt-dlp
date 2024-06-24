ee360f502dcf00313dcea3fab79504bb108e923d
bob <58648325+mrbob55@users.noreply.github.com>
2024-06-23 23:16:12 -0700

    Revert "[cleanup] Remove questionable extractors (#9911)"
    This reverts commit 01395a34345d1c6ba1b73ca92f94dd200dc45341.

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -317,6 +317,7 @@ from .businessinsider import BusinessInsiderIE
 from .buzzfeed import BuzzFeedIE
 from .byutv import BYUtvIE
 from .c56 import C56IE
+from .cableav import CableAVIE
 from .caffeinetv import CaffeineTVIE
 from .callin import CallinIE
 from .caltrans import CaltransIE
@@ -587,6 +588,7 @@ from .eggs import (
     EggsIE,
 )
 from .eighttracks import EightTracksIE
+from .einthusan import EinthusanIE
 from .eitb import EitbIE
 from .elementorembed import ElementorEmbedIE
 from .elonet import ElonetIE
@@ -909,6 +911,10 @@ from .iwara import (
 )
 from .ixigua import IxiguaIE
 from .izlesene import IzleseneIE
+from .jable import (
+    JableIE,
+    JablePlaylistIE,
+)
 from .jamendo import (
     JamendoAlbumIE,
     JamendoIE,
@@ -1581,6 +1587,7 @@ from .polskieradio import (
 )
 from .popcorntimes import PopcorntimesIE
 from .popcorntv import PopcornTVIE
+from .porn91 import Porn91IE
 from .pornbox import PornboxIE
 from .pornflip import PornFlipIE
 from .pornhub import (
@@ -2485,6 +2492,7 @@ from .wykop import (
 )
 from .xanimu import XanimuIE
 from .xboxclips import XboxClipsIE
+from .xfileshare import XFileShareIE
 from .xhamster import (
     XHamsterEmbedIE,
     XHamsterIE,
@@ -2548,6 +2556,8 @@ from .youporn import (
     YouPornTagIE,
     YouPornVideosIE,
 )
+from .yourporn import YourPornIE
+from .yourupload import YourUploadIE
 from .zaiko import (
     ZaikoETicketIE,
     ZaikoIE,
