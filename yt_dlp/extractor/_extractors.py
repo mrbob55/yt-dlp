58f76c06ecd5867ff515d3ca8085c5f387a083b0
bob <58648325+mrbob55@users.noreply.github.com>
2024-06-23 22:58:23 -0700

    Remove piracy filter.

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2272,10 +2272,6 @@ from .uliza import (
 from .umg import UMGDeIE
 from .unistra import UnistraIE
 from .unity import UnityIE
-from .unsupported import (
-    KnownDRMIE,
-    KnownPiracyIE,
-)
 from .uol import UOLIE
 from .uplynk import (
     UplynkIE,
