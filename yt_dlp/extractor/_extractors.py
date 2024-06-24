9a8325f43481b45dfa9463066d2602434e0fae3a
bob <58648325+mrbob55@users.noreply.github.com>
2024-06-23 23:25:50 -0700

    Revert "[ie/thisav] Remove (#8346)"
    This reverts commit cb480e390d85fb3a598c1b6d5eef3438ce729fc9.

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2075,6 +2075,7 @@ from .thestar import TheStarIE
 from .thesun import TheSunIE
 from .theweatherchannel import TheWeatherChannelIE
 from .thisamericanlife import ThisAmericanLifeIE
+from .thisav import ThisAVIE
 from .thisoldhouse import ThisOldHouseIE
 from .thisvid import (
     ThisVidIE,
