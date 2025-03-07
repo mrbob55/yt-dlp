cfa246434b550ef06d3dba688f60cf3be24cfd4d
bob <58648325+mrbob55@users.noreply.github.com>
2025-03-07 13:34:19 +0000

    Skip warnings when developing.

--- yt-dlp.sh
+++ yt-dlp.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-exec "${PYTHON:-python3}" -Werror -Xdev "$(dirname "$(realpath "$0")")/yt_dlp/__main__.py" "$@"
+# exec "${PYTHON:-python3}" -Werror -Xdev "$(dirname "$(realpath "$0")")/yt_dlp/__main__.py" "$@"
+exec "${PYTHON:-python3}" "$(dirname "$(realpath "$0")")/yt_dlp/__main__.py" "$@"
